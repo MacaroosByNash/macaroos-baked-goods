78a27a5f54379db209f64ca946aaff9e1a1f576e
joshysmart <sanijoshua10@gmail.com>
2024-10-18 22:55:05 +0100

    fix:hero

--- src/components/home/our-story.tsx
+++ src/components/home/our-story.tsx
@@ -1,20 +1,54 @@
+"use client";
+import { useRef, useState } from "react";
 import { PlayIcon } from "../icons";
 import Heading from "../shared/heading";
 import { useTranslations } from "next-intl";
+import { PauseIcon } from "lucide-react";
 
 export default function OurStory() {
   const t = useTranslations("STORY_SECTION");
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const handleClick = () => {
+    if (videoRef.current) {
+      if (isPlaying) {
+        videoRef.current.pause();
+      } else {
+        videoRef.current.play();
+      }
+      setIsPlaying(!isPlaying); // Toggle the play state
+    }
+  };
+
   return (
     <section className="story_section bg-primary flex items-center justify-center py-28 lg:py-40 px-4 md:px-8">
-      <div className="container flex flex-col lg:flex-row lg:items-center justify-between text-white">
-        <div>
-          <video src=""></video>
-          <button className="w-40 h-40 rounded-full bg-white flex items-center justify-center">
-            <PlayIcon />
-          </button>
+      <div className="container h-[375px] lg:h-auto flex flex-col lg:flex-row lg:items-center justify-between text-white">
+        <div className="relative flex items-center justify-center">
+          <div className="absolute -bottom-[180px] lg:left-20 w-[240px] h-[324px] lg:w-[274px] lg:h-[484px] ">
+            <video
+              ref={videoRef}
+              width="100%"
+              height="100%"
+              className="w-full h-full object-cover"
+              autoPlay={false}
+            >
+              <source
+                src="https://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4"
+                type="video/mp4"
+              />
+              Your browser does not support the video tag.
+            </video>
+            <button
+              onClick={handleClick}
+              className="absolute top-[100%] lg:top-[74%] -right-1/2 -translate-x-1/2 -translate-y-1/2 w-40 h-40 rounded-full bg-white flex items-center justify-center text-[#F5AE5C]"
+            >
+              {isPlaying ? <PauseIcon /> : <PlayIcon />}
+            </button>
+          </div>
         </div>
 
-        <div>
+        <div className="self-end">
           <Heading name={t("NAME")} title={t("TITLE")} />
         </div>
       </div>
